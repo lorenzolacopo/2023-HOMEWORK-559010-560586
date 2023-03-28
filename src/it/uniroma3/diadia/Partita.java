0275b4d6cf84414cfa6ece493ba7852ce3ff10fa
lorenzolacopo <129169772+lorenzolacopo@users.noreply.github.com>
2023-03-28 12:42:59 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/Partita.java
@@ -0,0 +1,82 @@
+package it.uniroma3.diadia;
+import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+/**
+ * Questa classe modella una partita del gioco
+ *
+ * @author  docente di POO
+ * @see Stanza
+ * @version base
+ */
+
+public class Partita {
+
+	
+	private Labirinto labirinto;
+	private Giocatore giocatore;
+	private boolean finita;
+	
+	
+	
+	public Partita(){
+	
+		
+		labirinto = new Labirinto();
+		labirinto.creaStanze();
+		
+		giocatore = new Giocatore();
+		this.finita = false;
+		
+	}
+
+   
+    
+    public Giocatore getGiocatore() {
+    	return giocatore;
+    }
+    public void setGiocatore(Giocatore giocatore) {
+    	this.giocatore = giocatore;
+    }
+    public Labirinto getLabirinto() {
+    	return labirinto;
+    }
+    public void setLabirinto(Labirinto labirinto) {
+    	this.labirinto = labirinto;
+    }
+
+
+	
+	
+	/**
+	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * @return vero se partita vinta
+	 */
+	public boolean vinta() {
+		return this.labirinto.getStanzaCorrente()== this.labirinto.getStanzaVincente();
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' finita
+	 * @return vero se partita finita
+	 */
+	public boolean isFinita() {
+		return finita || vinta() || (giocatore.getCfu() == 0);
+	}
+
+	/**
+	 * Imposta la partita come finita
+	 *
+	 */
+	public void setFinita() {
+		this.finita = true;
+	}
+
+
+
+	
+
+
+	
+}
